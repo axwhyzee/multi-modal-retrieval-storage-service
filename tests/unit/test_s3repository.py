1abeebae2f80c2d14a74a46ebd5bfdba937a3e1d
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-13 11:07:25 +0800

    write tests

---
+++ tests/unit/test_s3repository.py
@@ -0,0 +1,59 @@
+from typing import Dict
+
+import pytest
+
+from adapters.repository import AbstractRepository, DocPayload
+from domain.model import DocID
+
+
+class FakeS3Repository(AbstractRepository):
+    def __init__(self):
+        self._docs: Dict[str, DocPayload] = {}
+
+    def add(self, doc_id: DocID, doc_payload: DocPayload) -> None:
+        if doc_payload.content_type == "binary/octet-stream":
+            self._docs[doc_id] = doc_payload
+        else:
+            raise ValueError(f"Unrecognized content-type {doc_payload.content_type}")
+
+    def delete(self, doc_id: DocID) -> None:
+        self._docs.pop(doc_id)
+
+    def get(self, doc_id: DocID) -> DocPayload:
+        return self._docs[doc_id]
+
+
+@pytest.mark.parametrize(
+    "doc_id,doc_payload",
+    (
+        (
+            "test_doc.jpg",
+            DocPayload(data=bytes([0]), content_type="binary/octet-stream"),
+        ),
+    ),
+)
+def test_add_and_get_doc(
+    doc_id: DocID, doc_payload: DocPayload
+) -> None:
+    repo = FakeS3Repository()
+    repo.add(doc_id, doc_payload)
+    assert repo.get(doc_id) == doc_payload
+
+
+@pytest.mark.parametrize(
+    "doc_id,doc_payload",
+    (
+        (
+            "test_doc.jpg",
+            DocPayload(data=bytes([0]), content_type="binary/octet-stream"),
+        ),
+    ),
+)
+def test_del_non_existent_doc(
+    doc_id: DocID, doc_payload: DocPayload
+) -> None:
+    repo = FakeS3Repository()
+    repo.add(doc_id, doc_payload)
+    repo.delete(doc_id)
+    with pytest.raises(Exception) as e:
+        repo.delete(doc_id)
