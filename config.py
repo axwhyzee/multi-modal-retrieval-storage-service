ab98273b61f80396f02e9942c3191b22d54d881a
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-17 00:05:07 +0800

    remove bootstrap()

--- config.py
+++ config.py
@@ -1,25 +1,26 @@
 import os
-from pathlib import Path
+from typing import Any, Dict, TypeAlias
 
-from dotenv import find_dotenv, load_dotenv
+Config: TypeAlias = Dict[str, Any]
 
 
-def get_aws_bucket_name() -> str:
+def get_aws_s3_bucket_name() -> str:
     return os.environ["AWS_S3_BUCKET_NAME"]
 
 
-def _initialize_aws_creds():
-    AWS_CREDS_FILE_PATH = os.path.expanduser("~/.aws/credentials")
-    creds_file_path = Path(AWS_CREDS_FILE_PATH)
-    creds_file_path.parent.mkdir(exist_ok=True, parents=True)
-    creds_file_path.write_text(
-        f"[default]\n"
-        f'aws_access_key_id = {os.environ["AWS_S3_BUCKET_ACCESS_KEY"]}\n'
-        f'aws_secret_access_key = {os.environ["AWS_S3_BUCKET_SECRET_ACCESS_KEY"]}\n'
-        f'region = {os.environ["AWS_S3_BUCKET_REGION"]}'
-    )
+def get_redis_connection_params() -> Config:
+    return {
+        "host": os.environ["REDIS_HOST"],
+        "port": os.environ["REDIS_PORT"],
+        "username": os.environ["REDIS_USERNAME"],
+        "password": os.environ["REDIS_PASSWORD"],
+        "decode_responses": True,
+    }
 
 
-def bootstrap():
-    load_dotenv(find_dotenv())
-    _initialize_aws_creds()
+def get_aws_connection_params() -> Config:
+    return {
+        "aws_access_key_id": os.environ["AWS_S3_BUCKET_ACCESS_KEY"],
+        "aws_secret_access_key": os.environ["AWS_S3_BUCKET_SECRET_ACCESS_KEY"],
+        "region_name": os.environ["AWS_S3_BUCKET_REGION"],
+    }
