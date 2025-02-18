f98568eab1af09668f9cf4791fea474584151edd
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-18 17:35:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,16 +8,6 @@ def get_aws_s3_bucket_name() -> str:
     return os.environ["AWS_S3_BUCKET_NAME"]
 
 
-def get_redis_connection_params() -> Config:
-    return {
-        "host": os.environ["REDIS_HOST"],
-        "port": os.environ["REDIS_PORT"],
-        "username": os.environ["REDIS_USERNAME"],
-        "password": os.environ["REDIS_PASSWORD"],
-        "decode_responses": True,
-    }
-
-
 def get_aws_connection_params() -> Config:
     return {
         "aws_access_key_id": os.environ["AWS_S3_BUCKET_ACCESS_KEY"],
