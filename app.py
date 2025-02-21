14f23e8044a5cd8df36f448cc06a1af4788db4d5
Siah Wee Hung <siah_weehung@yahoo.com>
2025-02-21 22:15:24 +0800

    refactor service layer, add publish event with every add

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
+from event_core.adapters.exceptions import FailedToStore
 from flask import Flask, Response, request
 from flask_cors import CORS
 
-from repository import S3Repository
+from services.handlers import handle_add, handle_delete, handle_get
 
 app = Flask(__name__)
 CORS(app)
-
-repo = S3Repository()
 
 
 @app.route("/add", methods=["POST"])
 def add():
     if "file" not in request.files:
-        return "No file attached", 400
+        return "File required", 400
     if "key" not in request.form:
-        return "No object ID provided", 400
+        return "`key` provided", 400
+    if "obj_type" not in request.form:
+        return "`obj_type` required"
+
+    data = request.files["file"].read()
+    key = request.form["key"]
+    obj_type = request.form["obj_type"]
+
     try:
-        data = request.files["file"].read()
-        key = request.form["key"]
-        repo.add(data, key)
-    except Exception as e:
+        handle_add(data, key, obj_type)
+    except FailedToStore as e:
         return str(e), 400
-    return "Done", 200
+    except KeyError as e:
+        return f"Unsupported object type {obj_type}"
+    return "Success", 200
 
 
 @app.route("/get/<path:key>", methods=["GET"])
 def get(key: str):
-    data = repo.get(key)
-    return Response(data)
+    return Response(handle_get(key))
+
+
+@app.route("/delete/<path:key>", methods=["GET"])
+def delete(key: str):
+    handle_delete(key)
+    return "Success", 200
 
 
 if __name__ == "__main__":
---
+++ services/handlers.py
@@ -0,0 +1,37 @@
+from typing import Dict, Type
+
+from event_core.adapters.pubsub import RedisPublisher
+from event_core.domain.events import (
+    ChunkStored,
+    ChunkThumbnailStored,
+    DocStored,
+    DocThumbnailStored,
+    ObjStored,
+)
+from event_core.domain.types import ObjectType
+
+from adapters.repository import S3Repository
+
+repo = S3Repository()
+pub = RedisPublisher()
+
+EVENTS: Dict[str, Type[ObjStored]] = {
+    ObjectType.CHUNK: ChunkStored,
+    ObjectType.CHUNK_THUMBNAIL: ChunkThumbnailStored,
+    ObjectType.DOC: DocStored,
+    ObjectType.DOC_THUMBNAIL: DocThumbnailStored,
+}
+
+
+def handle_add(data: bytes, key: str, obj_type: str):
+    repo.add(data, key)
+    event = EVENTS[obj_type](obj_path=key)
+    pub.publish(event)
+
+
+def handle_get(key: str) -> bytes:
+    return repo.get(key)
+
+
+def handle_delete(key: str) -> None:
+    repo.delete(key)
