c732a68f989424d0a87af0f19c058dd47725d054
Siah Wee Hung <siah_weehung@yahoo.com>
2025-03-13 13:35:11 +0800

    revert commit

--- handlers.py
+++ handlers.py
@@ -5,7 +5,6 @@ from event_core.adapters.pubsub import AbstractPublisher
 from event_core.domain.events import (
     DocStored,
     DocThumbnailStored,
-    ElementStored,
     ElementThumbnailStored,
     ImageElementStored,
     ObjStored,
@@ -17,17 +16,16 @@ from event_core.domain.types import Asset, Element
 from bootstrap import DIContainer
 from repository import AbstractRepository
 
-ChannelT: TypeAlias = str
 EventT: TypeAlias = Type[ObjStored]
 RepoObjectT: TypeAlias = str
 
-PUB_CHANNELS_AND_EVENTS: Dict[RepoObjectT, Tuple[ChannelT, EventT]] = {
-    Asset.DOC.value: (DocStored.__name__, DocStored),
-    Asset.DOC_THUMBNAIL.value: (DocThumbnailStored.__name__, DocThumbnailStored,),
-    Asset.ELEM_THUMBNAIL.value: (ElementThumbnailStored.__name__, ElementThumbnailStored,),
-    Element.IMAGE.value: (ElementStored.__name__, ImageElementStored),
-    Element.PLOT.value: (ElementStored.__name__, PlotElementStored),
-    Element.TEXT.value: (ElementStored.__name__, TextElementStored),
+EVENTS: Dict[RepoObjectT, EventT] = {
+    Asset.DOC: DocStored,
+    Asset.DOC_THUMBNAIL: DocThumbnailStored,
+    Asset.ELEM_THUMBNAIL: ElementThumbnailStored,
+    Element.IMAGE: ImageElementStored,
+    Element.PLOT: PlotElementStored,
+    Element.TEXT: TextElementStored,
 }
 
 
@@ -40,9 +38,8 @@ def handle_add(
     pub: AbstractPublisher = Provide[DIContainer.pub],
 ):
     repo[key] = data
-    channel, event_cls = PUB_CHANNELS_AND_EVENTS[repo_obj_type]
-    event = event_cls(key=key)
-    pub.publish(event, channel=channel)
+    event = EVENTS[repo_obj_type](key=key)
+    pub.publish(event)
 
 
 @inject
