dc241e2323c3f82abd77922ecf0e50a264f1a49c
Siah Wee Hung <siah_weehung@yahoo.com>
2025-03-13 11:43:58 +0800

    bug fix: incorrect pub channel

--- handlers.py
+++ handlers.py
@@ -1,26 +1,33 @@
-from typing import Dict, List, Type
+from typing import Dict, List, Tuple, Type, TypeAlias
 
 from dependency_injector.wiring import Provide, inject
 from event_core.adapters.pubsub import AbstractPublisher
 from event_core.domain.events import (
     DocStored,
     DocThumbnailStored,
-    ElementThumbnailStored,
     ElementStored,
+    ElementThumbnailStored,
+    ImageElementStored,
     ObjStored,
+    PlotElementStored,
+    TextElementStored,
 )
 from event_core.domain.types import Asset, Element
 
 from bootstrap import DIContainer
 from repository import AbstractRepository
 
-EVENTS: Dict[str, Type[ObjStored]] = {
-    Asset.DOC: DocStored,
-    Asset.DOC_THUMBNAIL: DocThumbnailStored,
-    Asset.ELEM_THUMBNAIL: ElementThumbnailStored,
-    Element.IMAGE: ElementStored,
-    Element.PLOT: ElementStored,
-    Element.TEXT: ElementStored,
+ChannelT: TypeAlias = str
+EventT: TypeAlias = Type[ObjStored]
+RepoObjectT: TypeAlias = str
+
+PUB_CHANNELS_AND_EVENTS: Dict[RepoObjectT, Tuple[ChannelT, EventT]] = {
+    Asset.DOC.value: (DocStored.__name__, DocStored),
+    Asset.DOC_THUMBNAIL.value: (DocThumbnailStored.__name__, DocThumbnailStored,),
+    Asset.ELEM_THUMBNAIL.value: (ElementThumbnailStored.__name__, ElementThumbnailStored,),
+    Element.IMAGE.value: (ElementStored.__name__, ImageElementStored),
+    Element.PLOT.value: (ElementStored.__name__, PlotElementStored),
+    Element.TEXT.value: (ElementStored.__name__, TextElementStored),
 }
 
 
@@ -33,8 +40,9 @@ def handle_add(
     pub: AbstractPublisher = Provide[DIContainer.pub],
 ):
     repo[key] = data
-    event = EVENTS[repo_obj_type](key=key)
-    pub.publish(event)
+    channel, event_cls = PUB_CHANNELS_AND_EVENTS[repo_obj_type]
+    event = event_cls(key=key)
+    pub.publish(event, channel=channel)
 
 
 @inject
